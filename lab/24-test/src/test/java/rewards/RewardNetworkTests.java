05d1e43404f950974340534cfb1489214434d9c4
eucarizan <eucarizan@gmail.com>
2024-07-09 19:48:07 +0800

    refactor: test, todo-01 done

--- lab/24-test/src/test/java/rewards/RewardNetworkTests.java
+++ lab/24-test/src/test/java/rewards/RewardNetworkTests.java
@@ -1,13 +1,10 @@
 package rewards;
 
 import common.money.MonetaryAmount;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,24 +12,6 @@ import static org.junit.jupiter.api.Assertions.*;
  * A system test that verifies the components of the RewardNetwork
  * application work together to reward for dining successfully.
  * It uses Spring to bootstrap the application for use in a test environment.
- */
-
-/*
- * TODO-00: In this lab, you are going to exercise the following:
- * - Using annotation(s) from Spring TestContext Framework for
- *   creating application context for the test
- * - Using profiles in the test
- *
- * TODO-01: Use Spring TestContext Framework
- * - Read through Spring document on Spring TestContext Framework
- *   (https://docs.spring.io/spring-framework/docs/current/spring-framework-reference/testing.html#testcontext-framework)
- * - Add annotation(s) to this class so that it can
- *   use Spring TestContext Framework
- * - Remove setUp() and tearDown() methods
- * - Remove the attribute "context" which is not needed anymore.
- * - Run the current test. Observe a test failure.
- * - Use @Autowired to populate the rewardNetwork bean.
- * - Re-run the current test, it should pass.
  */
 
 /* TODO-02: Annotate all 'Stub*Repository' classes with @Repository
@@ -83,58 +62,46 @@ import static org.junit.jupiter.api.Assertions.*;
  *   lab document.)
  * - Run the test again.
  */
+@SpringJUnitConfig(classes = TestInfrastructureConfig.class)
 
 public class RewardNetworkTests {
 
+    @Autowired
     private RewardNetwork rewardNetwork;
-	private ConfigurableApplicationContext context;
-
-    @BeforeEach
-    public void setUp() {
-        context = SpringApplication.run(TestInfrastructureConfig.class);
-        rewardNetwork = context.getBean(RewardNetwork.class);
-    }
-
-	@AfterEach
-	public void tearDown() throws Exception {
-		if (context != null) {
-			context.close();
-		}
-	}
 
     @Test
-	@DisplayName("Test if reward computation and distribution works")
+    @DisplayName("Test if reward computation and distribution works")
     public void testRewardForDining() {
-		// create a new dining of 100.00 charged to credit card '1234123412341234' by merchant '123457890' as test input
-		Dining dining = Dining.createDining("100.00", "1234123412341234", "1234567890");
+        // create a new dining of 100.00 charged to credit card '1234123412341234' by merchant '123457890' as test input
+        Dining dining = Dining.createDining("100.00", "1234123412341234", "1234567890");
 
-		// call the 'rewardNetwork' to test its rewardAccountFor(Dining) method
-		// this fails if you have selected an account without beneficiaries!
-		RewardConfirmation confirmation = rewardNetwork.rewardAccountFor(dining);
+        // call the 'rewardNetwork' to test its rewardAccountFor(Dining) method
+        // this fails if you have selected an account without beneficiaries!
+        RewardConfirmation confirmation = rewardNetwork.rewardAccountFor(dining);
 
-		// assert the expected reward confirmation results
-		assertNotNull(confirmation);
-		assertNotNull(confirmation.getConfirmationNumber());
+        // assert the expected reward confirmation results
+        assertNotNull(confirmation);
+        assertNotNull(confirmation.getConfirmationNumber());
 
-		// assert an account contribution was made
-		AccountContribution contribution = confirmation.getAccountContribution();
-		assertNotNull(contribution);
+        // assert an account contribution was made
+        AccountContribution contribution = confirmation.getAccountContribution();
+        assertNotNull(contribution);
 
-		// the contribution account number should be '123456789'
-		assertEquals("123456789", contribution.getAccountNumber());
+        // the contribution account number should be '123456789'
+        assertEquals("123456789", contribution.getAccountNumber());
 
-		// the total contribution amount should be 8.00 (8% of 100.00)
-		assertEquals(MonetaryAmount.valueOf("8.00"), contribution.getAmount());
+        // the total contribution amount should be 8.00 (8% of 100.00)
+        assertEquals(MonetaryAmount.valueOf("8.00"), contribution.getAmount());
 
-		// the total contribution amount should have been split into 2 distributions
-		assertEquals(2, contribution.getDistributions().size());
+        // the total contribution amount should have been split into 2 distributions
+        assertEquals(2, contribution.getDistributions().size());
 
-		// The total contribution amount should have been split into 2 distributions
-		// each distribution should be 4.00 (as both have a 50% allocation).
-		// The assertAll() is from JUnit 5 to group related checks together.
-		assertAll("distribution of reward",
-				() -> assertEquals(2, contribution.getDistributions().size()),
-				() -> assertEquals(MonetaryAmount.valueOf("4.00"), contribution.getDistribution("Annabelle").getAmount()),
-				() -> assertEquals(MonetaryAmount.valueOf("4.00"), contribution.getDistribution("Corgan").getAmount()));
+        // The total contribution amount should have been split into 2 distributions
+        // each distribution should be 4.00 (as both have a 50% allocation).
+        // The assertAll() is from JUnit 5 to group related checks together.
+        assertAll("distribution of reward",
+                () -> assertEquals(2, contribution.getDistributions().size()),
+                () -> assertEquals(MonetaryAmount.valueOf("4.00"), contribution.getDistribution("Annabelle").getAmount()),
+                () -> assertEquals(MonetaryAmount.valueOf("4.00"), contribution.getDistribution("Corgan").getAmount()));
     }
 }
